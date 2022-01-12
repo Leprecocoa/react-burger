e71e28dc58b36efd4e20bd4ce53bd5f0837fb3f1
Anna Chirva <anna.chirva.hr@yandex.ru>
2022-01-12 13:38:43 +0300

    Переименовать компоненты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
-import Header from "../header/header.js";
+import Header from "../header/header";
 import appstyles from "./app.module.css";
-import Main from "../main/main.js";
+import Main from "../main/main";
 
 function App() {
   return (
